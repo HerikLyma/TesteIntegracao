e3a4aa5f14095967a2590f9ad967ce328c61c287
HerikLyma <heriklyma@hotmail.com>
2017-01-29 11:28:55 -0200

    Redução da cobertura de testes

--- Teste/tst_hello.cpp
+++ Teste/tst_hello.cpp
@@ -12,7 +12,7 @@ private Q_SLOTS:
         QVERIFY2(Hello(1).say() == "Hello", "Deve exibir Hello");
         QVERIFY2(Hello(2).say() == "Hello ", "Deve exibir Hello ");
         QVERIFY2(Hello(3).say() == "Hello World", "Deve exibir Hello World");
-        QVERIFY2(Hello(4).say() == "Hello World!!!", "Deve exibir Hello World!!!");
+        //QVERIFY2(Hello(4).say() == "Hello World!!!", "Deve exibir Hello World!!!");
     }
 
 };
